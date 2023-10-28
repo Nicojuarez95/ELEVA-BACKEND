e3a97721e12322efde4eeb7676c8ad1e399d1892
Nico <juareznico61@gmail.com>
2023-10-28 16:16:21 -0300

    proyecto

--- config/mailer.js
+++ config/mailer.js
@@ -40,8 +40,7 @@ async function createMailTransporter(user) {
         </td>
         </tr>
         </tbody></table>
-         
-        
+          
         
         <table border="0" cellpadding="0" cellspacing="0" class="m_-5401722008193577908deviceWidth" style="max-width:600px" width="100%">
         <tbody><tr>
