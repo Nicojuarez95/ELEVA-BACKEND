5be42b3bb234e5c1376139557c23855569e40843
Nico <juareznico61@gmail.com>
2023-10-28 16:13:09 -0300

    proyecto

--- middlewares/jwtmiddleware.js
+++ middlewares/jwtmiddleware.js
@@ -4,7 +4,6 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(' ')[1]
 
-
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.SECRET, (err, user) => {
