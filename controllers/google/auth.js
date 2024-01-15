4cd8163e377138ce9ebf50a6978bacbe6ecbe3ea
julianramos42 <julianete4@gmail.com>
2024-01-15 17:42:47 -0300

    google login fix

--- controllers/google/auth.js
+++ controllers/google/auth.js
@@ -4,21 +4,24 @@ import bcryptjs from 'bcryptjs'
 import jsonwebtoken from 'jsonwebtoken'
 
 export default async function googleSignIn(req, res, next) {
-    req.body.is_online = false
-    req.body.is_admin = false
-    req.body.is_seller = false
-    req.body.is_verified = true
-    req.body.verify_code = Crypto.randomBytes(10).toString('hex')
-    req.body.password = bcryptjs.hashSync(req.body.password, 10)
     try {
 
         const user = await User.findOne({ email: req.body.email })
         if (user) {
-            const db_pass = user.password
+            req.user = {
+                id: user._id,
+                email: user.email,
+                password: user.password,
+                is_admin: user.is_admin,
+                is_seller: user.is_seller,
+                is_verified: user.is_verified
+            }
+            const db_pass = req.user.password
             const form_pass = req.body.password
+
             if (bcryptjs.compareSync(form_pass, db_pass)) {
                 let loginUser = await User.findOneAndUpdate(
-                    { email: user.email }, //parametro de busqueda
+                    { email: req.user.email }, //parametro de busqueda
                     { is_online: true }, //parámetro a modificar
                     { new: true } //para que devuelva el objeto modificado
                 )
@@ -29,18 +32,25 @@ export default async function googleSignIn(req, res, next) {
                     { expiresIn: 60 * 60 * 24 * 7 }
                 )
                 return res.status(200).json({
-                    succes: true,
+                    success: true,
                     message: 'Conectado!',
                     loginUser,
                     token
                 })
             } else {
                 return res.status(400).json({
-                    succes: false,
+                    success: false,
                     message: 'Datos incorrectos!'
                 })
             }
         } else {
+            req.body.is_online = false
+            req.body.is_admin = false
+            req.body.is_seller = false
+            req.body.is_verified = true
+            req.body.verify_code = Crypto.randomBytes(10).toString('hex')
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+            
             const user = await User.create(req.body)
             let loginUser = await User.findOneAndUpdate(
                 { email: user.email }, //parametro de busqueda
