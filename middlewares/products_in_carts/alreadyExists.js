c10217ebd4079e9de8e53d0f0269a434273fe326
julianramos42 <julianete4@gmail.com>
2024-01-18 15:31:29 -0300

    arreglo al carrito

--- middlewares/products_in_carts/alreadyExists.js
+++ middlewares/products_in_carts/alreadyExists.js
@@ -1,35 +1,47 @@
-import Shop from '../../models/Shop.js'
-import Product_in_carts from '../../models/Product_in_carts.js'
+import Shop from '../../models/Shop.js';
+import Product_in_carts from '../../models/Product_in_carts.js';
 
 async function alreadyExists(req, res, next) {
-        try{
-            const shop = await Shop.findOne({ _id: req.params.id })
-            if (shop){
+    try {
+        const shop = await Shop.findOne({ _id: req.params.id });
 
-                const existsProduct = await Product_in_carts.findOne({ title: req.body.title, store_id: shop._id})
-                
-                if (existsProduct){
-                    let quantity = existsProduct.quantity + req.body.quantity
-                    const product = await Product_in_carts.findOneAndUpdate(
-                        { _id: existsProduct._id },
-                        {quantity: quantity },
-                        {new:true})
-                    return res.status(200).json({
-                        success: true,
-                        message: "El producto ya existe, se agrega la cantidad "
-                        })
-                }else{
-                    next()
-                }
-            }else{
-                return res.status(404).json({
-                    success: false,
-                    message: "Tienda no encontrada"
-                    })
-            }
-        }catch(error){
-            next(error)
+        if (!shop) {
+            return res.status(404).json({
+                success: false,
+                message: "Tienda no encontrada"
+            });
         }
+
+        const title = req.body.title;
+        const user_id = req.user._id;
+        
+        const existingProduct = await Product_in_carts.findOne({
+            title,
+            store_id: shop._id,
+            user_id
+        });
+
+        if (existingProduct) {
+
+            let quantity = existingProduct.quantity + req.body.quantity;
+
+            const updatedProduct = await Product_in_carts.findOneAndUpdate(
+                { _id: existingProduct._id },
+                { quantity },
+                { new: true }
+            );
+
+            return res.status(200).json({
+                success: true,
+                message: "El producto ya existe en tu carrito, se agrega la cantidad",
+                data: updatedProduct
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
 }
 
-export default alreadyExists
+export default alreadyExists;
